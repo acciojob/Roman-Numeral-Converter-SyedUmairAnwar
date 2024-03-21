691808212fb84f92e0a68b8bdea5518b353db64e
syed <71326007+SyedUmairAnwar@users.noreply.github.com>
2024-03-21 17:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,9 +8,21 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
+    let str="";
+	if (num>1000){
+		str=str+'M';
+		num=num-1000;
+	}
+	 if(num>500){
+		str=str+'D';num=num-500;
+	}
+	while (num>100){str=str+'C';num=num-100;}
+	while (num>50){str=str+'L';num=num-50;}
+	while  (num>10){str=str+'X';num=num-10;}
+	while  (num>5){str=str+'V';num=num-5;}
+	while  (num>0){str=str+'I';num=num-1;}
   //your code here
-
+console.log(str)
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
